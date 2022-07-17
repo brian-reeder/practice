c328675c9fd519c2c4ee0cc2fb294282fffe9f00
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-07-17 10:12:46 -0500

    03-number-of-1-bits
    +Practice problem 03 for leetcode study plan

---
+++ leetcode/Study_Plan-Programming_Skills/03-number-of-1-bits/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+File Name: 
+    03-number-of-1-bits/Program.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    Sunday, July 17, 2022, 14:39:26
+
+Description:
+    Write a function that takes an unsigned integer and returns the number of 
+        '1' bits it has (also known as the Hamming weight).
+
+Example 1:
+    Input: n = 00000000000000000000000000001011
+    Output: 3
+    Explanation: The input binary string 00000000000000000000000000001011 has
+        a total of three '1' bits.
+Example 2:
+    Input: n = 00000000000000000000000010000000
+    Output: 1
+    Explanation: The input binary string 00000000000000000000000010000000 has 
+        a total of one '1' bit.
+Example 2:
+    Input: n = 11111111111111111111111111111101
+    Output: 1
+    Explanation: The input binary string 11111111111111111111111111111101 has 
+        a total of thirty one '1' bits.
+Constraints:
+    The input must be a binary string of length 32.
+*/
+namespace NumberOf1Bits {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Number of 1 bits");
+            var testcases = new (uint, int)[] {
+                 (0b00000000000000000000000000001011,  3)
+                ,(0b00000000000000000000000010000000,  1)
+                ,(0b11111111111111111111111111111101, 31)
+            };
+
+            Solution solve = new Solution();
+            foreach((uint, int)t in testcases) {
+                int x = solve.HammingWeight(t.Item1);
+                
+                Console.WriteLine($"Input: n={t.Item1}");
+                Console.WriteLine($"Output: {x}");
+                Console.WriteLine($"Pass: {x == t.Item2}");
+                Console.WriteLine();
+            }
+            return;
+        }
+    }
+}
---
+++ leetcode/Study_Plan-Programming_Skills/03-number-of-1-bits/Solution.cs
@@ -0,0 +1,28 @@
+/*
+File Name: 
+    02-average-salary-excluding-the-minimum-and-maximum-salary/Solution.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    Saturday, July 16, 2022, 18:50:00
+
+Description:
+    Write a function that takes an unsigned integer and returns the number of 
+        '1' bits it has (also known as the Hamming weight). This solution
+        adds the bitwise & result of the input value and the 2^0 place. Then
+        the input value is shifted down 1 place.
+*/
+public class Solution {
+    public int HammingWeight(uint n) {
+        var count = 0;
+        // Shift all bits 1 position to the right
+        // Continue until there are no more bits
+        for(;n > 0; n = n >> 1)
+            // Count the 2^0 bit
+            count += Convert.ToInt16(n & 1);
+        
+        return count;
+    }
+}
