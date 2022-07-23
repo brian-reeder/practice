e29920d5576188bab0cd52004810dfb34cc1619f
Brian <14955584+brian-reeder@users.noreply.github.com>
2022-07-23 08:59:12 -0500

    19-merge-strings-alternately
    + Solution for problem 19 from leetcode study path.

---
+++ leetcode/Study_Plan-Programming_Skills/19-merge-strings-alternately/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+File Name: 
+    19-merge-strings-alternately/Program.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    July 23, 2022, 8:35:51
+
+Description:
+    You are given two strings word1 and word2. Merge the strings by adding 
+    letters in alternating order, starting with word1. If a string is longer
+    than the other, append the additional letters onto the end of the merged
+    string.
+
+    Return the merged string.
+*/
+namespace ReshapeTheMatrix {
+    public class Program {
+        public static void Main(string[] args) {
+            // Load test cases
+            var testcases = new (string, string, string)[] {
+                 ("abc", "pqr", "apbqcr")
+                ,("ab", "pqrs", "apbqrs")
+                ,("abcd", "pq", "apbqcd")
+                ,("", "wxyz", "wxyz")
+                ,("abcd", "", "abcd")
+                ,("", "", "")
+            };
+
+            var solve = new Solution();
+            foreach((string, string, string) t in testcases) {
+                Console.WriteLine($"Input: word1 = \"{t.Item1}\", word2 = \"{t.Item2}\" ");
+                var x = solve.MergeString(t.Item1, t.Item2);
+                Console.WriteLine($"Output: \"{x}\"");
+
+                // Check output against the test
+                Console.WriteLine($"Pass: {t.Item3.Equals(x)}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ leetcode/Study_Plan-Programming_Skills/19-merge-strings-alternately/Solution.cs
@@ -0,0 +1,34 @@
+/*
+File Name: 
+    19-merge-strings-alternately/Solution.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    July 23, 2022, 8:36:00
+
+Description:
+    You are given two strings word1 and word2. Merge the strings by adding 
+    letters in alternating order, starting with word1. If a string is longer
+    than the other, append the additional letters onto the end of the merged
+    string.
+
+    Return the merged string.
+*/
+public class Solution {
+    public string MergeString(string word1, string word2) {
+        var newString = new char[word1.Length + word2.Length];
+        var i1 = 0;
+        var i2 = 0;
+        for(int i = 0; i < newString.Length;) {
+            if(i1 < word1.Length)
+                newString[i++] = word1[i1++];
+
+            if(i2 < word2.Length)
+                newString[i++]  = word2[i2++];
+        }
+
+        return new String(newString);
+    }
+}
