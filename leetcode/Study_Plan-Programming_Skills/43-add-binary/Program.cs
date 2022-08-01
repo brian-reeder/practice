b3826f9dcb146305be32ea173336a9dc739240bd
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-08-01 12:20:35 -0500

    43-add-binary
    + Solution for problem 43 of leetcode study skills path.

---
+++ leetcode/Study_Plan-Programming_Skills/43-add-binary/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+File Name: 
+    43-add-binary/Program.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    August 1, 2022, 11:40:44
+
+Description:
+    Given two binary strings a and b, return their sum as a binary string.
+
+Constraints:
+    1 <= a.length, b.length <= 104
+    a and b consist only of '0' or '1' characters.
+    Each string does not contain leading zeros except for the zero itself.
+*/
+namespace AddBinary {
+    public class Program {
+        public static void Main(string[] args) {
+            var testcases = new (string, string, string)[] {
+                 ("0", "1", "1")
+                ,("1", "0", "1")
+                ,("1", "1", "10")
+                ,("10", "1", "11")
+                ,("1", "10", "11")
+                ,("10", "10", "100")
+                ,("11", "11", "110")
+                ,("1010", "1011", "10101")
+            };
+
+            var solve = new Solution();
+            foreach(var test in testcases) {
+                var output = solve.AddBinary(test.Item1, test.Item2);
+                Console.WriteLine($"Input: a = \"{test.Item1}\", b = \"{test.Item2}\"");
+                Console.WriteLine($"Output: \"{output}\"");
+                Console.WriteLine($"Pass: {test.Item3.SequenceEqual(output)}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ leetcode/Study_Plan-Programming_Skills/43-add-binary/Solution.cs
@@ -0,0 +1,49 @@
+/*
+File Name: 
+    43-add-binary/Solution.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    August 1, 2022, 11:41:09
+
+Description:
+    Given two binary strings a and b, return their sum as a binary string.
+
+Constraints:
+    1 <= a.length, b.length <= 104
+    a and b consist only of '0' or '1' characters.
+    Each string does not contain leading zeros except for the zero itself.
+*/
+public class Solution {
+    public string AddBinary(string a, string b) {
+        int rounds = Math.Max(a.Length, b.Length);
+        var cBuff = new char[rounds+1];
+        var start = rounds;
+        for(int i = 1; i <= rounds; i++) {
+            var cIndex = cBuff.Length-i;
+            if(i <= a.Length)
+                cBuff[cIndex] += (char)(a[a.Length-i] - '0');
+            if(i <= b.Length)
+                cBuff[cIndex] += (char)(b[b.Length-i] - '0');
+
+            cBuff[cIndex-1] += (char)(cBuff[cIndex] / 2);
+            cBuff[cIndex] = (char)('0' + cBuff[cIndex] % 2);
+            if(cBuff[cIndex-1] != 0)
+                start = cIndex-1;
+            else if (cIndex < start && cBuff[cIndex] != 0)
+                start = cIndex;
+        }
+        cBuff[0] += '0';
+/*
+        int start = cBuff.Length-1;
+        for(int i = start; i >= 0; i--) {
+            cBuff[i] += '0';
+            if(cBuff[i] != '0')
+                start = i;
+        }
+*/        
+        return new string(cBuff[start..]);
+    }
+}
