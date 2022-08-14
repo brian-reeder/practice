19e3f8a6a42b3872d1488332a36ff64ccaffcb6c
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-08-14 12:11:28 -0500

    70-flatten-nested-list-iterator
    + Use a stack to track the current list state.
    + Flatten elements on the stack until the top element is an integer.

---
+++ leetcode/Study_Plan-Programming_Skills/70-flatten-nested-list-iterator/Solution.cs
@@ -0,0 +1,26 @@
+﻿/*
+File Name: 
+    70-flatten-nested-list-iterator/Solution.cs
+FileType: 
+    Visual C# Source file
+Author:
+    https://github.com/brian-reeder
+Created On:
+    August 14, 2022, 17:09
+
+Description:
+    You are given a nested list of integers nestedList. Each element is either
+    an integer or a list whose elements may also be integers or other lists.
+    Implement an iterator to flatten it.
+
+    Implement the NestedIterator class:
+    * NestedIterator(List<NestedInteger> nestedList) Initializes the iterator
+        with the nested list nestedList.
+    * int next() Returns the next integer in the nested list.
+    * boolean hasNext() Returns true if there are still some integers in the
+        nested list and false otherwise.
+
+Constraints:
+    * 1 <= nestedList.length <= 500
+    * The values of the integers in the nested list is in the range [-10^6, 10^6].
+*/
